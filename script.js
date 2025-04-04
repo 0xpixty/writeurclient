f0a54bad442eef7202415c08e2873e5d295d4410
0xpixty <pixtyy@vk.com>
2025-04-04 20:49:13 +0300

    4

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function showContent(id) {
     });
     document.getElementById(id).classList.add('active');
 }
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.left = Math.random() * 100 + 'vw';
+    star.style.top = Math.random() * -20 + 'vh';
+    star.style.animationDuration = (Math.random() * 1 + 1) + 's';
+    document.body.appendChild(star);
+    setTimeout(() => star.remove(), 2000);
+}
+
+setInterval(createStar, 200);
